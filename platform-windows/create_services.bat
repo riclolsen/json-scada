b6b2c59ed08a6677c21f062b4e951812625a72ad
Ricardo Olsen <olsen.ricardo@gmail.com>
2020-09-10 12:12:22 -0300

    Windows create services template updated.

--- platform-windows/create_services.bat
+++ platform-windows/create_services.bat
@@ -8,7 +8,15 @@ cd \json-scada\bin
 C:\json-scada\postgresql-runtime\bin\pg_ctl.exe register -N JSON_SCADA_postgresql -D "C:\json-scada\postgresql-data"
 
 nssm install JSON_SCADA_mongodb "C:\json-scada\mongodb-runtime\bin\mongod.exe" --config  "c:\json-scada\mongodb-runtime\bin\mongod.cfg" 
-nssm install JSON_SCADA_calculations "C:\json-scada\bin\calculations.exe"
-nssm install JSON_SCADA_process_dumpdb "C:\json-scada\sql\process_pg_dumpdb.bat"
+nssm install JSON_SCADA_calculations "C:\json-scada\bin\calculations.exe" 1 1 "c:\json-scada\json-scada.json"
+
+nssm install JSON_SCADA_cs_data_processor "c:\Program Files\nodejs\node.exe" "C:\json-scada\src\cs_data_processor\cs_data_processor.js" 1 1 "c:\json-scada\json-scada.json"
+nssm set AppDirectory JSON_SCADA_cs_data_processor  "C:\json-scada\src\cs_data_processor"
+nssm install JSON_SCADA_server_realtime  "c:\Program Files\nodejs\node.exe" "C:\json-scada\src\server_realtime\index.js" 1 1 "c:\json-scada\json-scada.json"
+nssm set AppDirectory JSON_SCADA_server_realtime "C:\json-scada\src\server_realtime"
+
+nssm install JSON_SCADA_process_rtdata "C:\json-scada\sql\process_pg_rtdata.bat"
+nssm set JSON_SCADA_process_rtdata AppDirectory "C:\json-scada\sql"
 nssm install JSON_SCADA_process_hist "C:\json-scada\sql\process_pg_hist.bat"
+nssm set JSON_SCADA_process_hist AppDirectory "C:\json-scada\sql"
 
