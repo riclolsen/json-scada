e011b3755b4db8ae7d35c5a70ef78002edcdf196
Ricardo Olsen <olsen.ricardo@gmail.com>
2020-09-23 18:30:10 -0300

    Mongo seed updated with users and roles.

--- mongo_seed/init.sh
+++ mongo_seed/init.sh
@@ -3,3 +3,5 @@ JSON_SCADA_DATABASE=json_scada
 mongo mongodb://localhost:27017/$JSON_SCADA_DATABASE  < a_rs-init.js
 mongo mongodb://localhost:27017/$JSON_SCADA_DATABASE  < b_create-db.js
 mongoimport --db $JSON_SCADA_DATABASE --collection realtimeData --type json --file realtime_data.json 
+mongoimport --db $JSON_SCADA_DATABASE --collection roles --type json --file roles.json 
+mongoimport --db $JSON_SCADA_DATABASE --collection users --type json --file users.json 
