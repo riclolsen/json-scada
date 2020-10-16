2694055975ac39c36b6de17cd6f55b8bf5e95770
Ricardo Olsen <olsen.ricardo@gmail.com>
2020-10-16 12:08:40 -0300

    Admin Management Vue/Vuetify web app initial commit.

---
+++ src/htdocs-admin/src/components/AppNavigation.vue
@@ -0,0 +1,43 @@
+<template>
+    <span>
+        <v-navigation-drawer app v-model="drawer" class="primary lighten-2" dark disable-resize-watcher>
+            <v-list>
+                <template v-for="(item, index) in items">
+                    <v-list-item :key="index">
+                        <v-list-item-content>
+                            {{item.title}}
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-divider :key="`divider-${index}`"></v-divider>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar color="primary darken-4" dark>
+            <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-spacer class="hidden-md-and-up"></v-spacer>
+            <img alt="Vue logo" height="50px" src="../assets/json-scada.svg">
+            <v-spacer></v-spacer>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn text class="hidden-sm-and-down">LOGOUT <v-icon>mdi-logout</v-icon> </v-btn>
+        </v-app-bar>
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Admin Management',
+            drawer: false,
+            items: [
+                { title: 'Logout' }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/htdocs-admin/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TabbedOptions />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TabbedOptions from '@/components/TabbedOptions.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TabbedOptions
+  }
+}
+</script>
