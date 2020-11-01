416d6b80af9964728d5afe1a0c3d23d4ca1e3acc
joyshmitz <jshmitz@me.com>
2020-11-01 15:32:01 +0200

    add screen_list_ua index_ua

---
+++ src/htdocs/svg/screen_list_ua.js
@@ -0,0 +1,19 @@
+// screen_list.js
+// Warning:
+// There can't be any spaces after the "\" character in the lines below
+
+optionhtml = "\
+<option id='SELTELA_OPC1' selected disabled='disabled'>Choose a screen ...</option>\
+<optgroup label='Підстанція 1'>\
+  <option value='../svg/kaw2.svg' >KAW2 ............[1]{A}</option>\
+</optgroup>\
+<optgroup label='Підстанція 2'>\
+  <option value='../svg/kor1.svg' >KOR1.............[2]{O}</option>\
+</optgroup>\
+<optgroup label='Підстанція 3'>\
+  <option value='../svg/knh2.svg' >KNH2.............[3]{N}</option>\
+</optgroup>\
+<optgroup label='Підстанція 4'>\
+  <option value='../svg/kik3.svg' >KIK3.............[4]{I}</option>\
+</optgroup>\
+";
