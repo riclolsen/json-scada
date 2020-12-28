8a338e3c3b948a6a358ca69255320667349148c5
Ricardo Olsen <olsen.ricardo@gmail.com>
2020-12-28 10:37:46 -0300

    Admin manager UI updated.

--- src/server_realtime_auth/app/models/protocolDriverInstance.model.js
+++ src/server_realtime_auth/app/models/protocolDriverInstance.model.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+const Double = require('@mongoosejs/double');
 
 const ProtocolDriverInstance = mongoose.model(
   "ProtocolDriverInstance",
   new mongoose.Schema({
     protocolDriver: {type: String, default: "UNDEFINED"},
-    protocolDriverInstanceNumber: {type: Number, default: 1.0},
+    protocolDriverInstanceNumber: {type: Double, default: 1.0},
     enabled: {type: Boolean, default: true},
-    logLevel: {type: Number, default: 1.0},
+    logLevel: {type: Double, default: 1.0},
     nodeNames: {type: [String], default: ["mainNode"]},
     keepProtocolRunningWhileInactive: {type: Boolean, default: false},
     //activeNodeName: {type: String, default: ""},
