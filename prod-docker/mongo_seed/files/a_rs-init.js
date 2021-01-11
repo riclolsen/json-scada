1affeec61e332601a35a73de92e3d8851b801c50
joyshmitz <jshmitz@me.com>
2021-01-11 14:57:05 +0200

    add prod-docker

---
+++ prod-docker/mongo_seed/files/a_rs-init.js
@@ -0,0 +1,9 @@
+rs.initiate(
+    {
+      _id : 'rs1',
+      members: [
+        { _id : 0, host : "jsdemo_mongorsn1:27017" }
+      ]
+    }
+  )
+  
---
+++ prod-docker/postgres_seed/init-user-db.sh
@@ -0,0 +1,1 @@
+psql -h 127.0.0.1 -U postgres -w -f create_tables.sql
