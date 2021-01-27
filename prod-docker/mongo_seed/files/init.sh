4e5cdd18a1acc046429e79ffd0f83eb1dca35f58
joyshmitz <jshmitz@me.com>
2021-01-27 18:05:07 +0200

    make prod-docker dir and copy demo-docker

---
+++ prod-docker/mongo_seed/files/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+sleep 5 
+mongoimport --db $MONGO_INITDB_DATABASE --collection protocolDriverInstances --type json --file /docker-entrypoint-initdb.d/demo_instances.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection protocolConnections --type json --file /docker-entrypoint-initdb.d/demo_connections.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection realtimeData --type json --file /docker-entrypoint-initdb.d/demo_data.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection processInstances --type json --file /docker-entrypoint-initdb.d/demo_process_instances.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection users --type json --file /docker-entrypoint-initdb.d/demo_users.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection roles --type json --file /docker-entrypoint-initdb.d/demo_roles.json 
---
+++ prod-docker/sql/process_pg_rtdata.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+# process sql files in a postgresql database then removes the files
+
+# set defaults
+psqlPath=${PSQL_PATH:-"/usr/bin"}
+dbHost=${PGHOST:-"127.0.0.1"}
+dbPort=${PGPORT:-5432}
+dbName=${PGDATABASE:-"json_scada"}
+dbUser=${PGUSER:-"json_scada"}
+# PGPASSWORD=${PGPASSWORD :-""}
+
+cd /sql
+
+# exit when flock fails
+set -e
+
+(
+# locks to avoid multiples of this process running
+# flock -n -x 10 9
+
+# avoids exit in case of errors
+set +e
+
+while [ 1 ]; do
+
+# gets all sql rtdata files
+  for file in pg_rtdata_*.sql; do
+  if [ "$file" != "pg_rtdata_*.sql" ]; then
+
+# process sql file into the database
+    res=`$psqlPath/psql -h $dbHost -U "$dbUser" -d $dbName -p $dbPort < "$file" `
+
+    if [ "$?" = "0" ]; then
+
+# if ok, deletes the sql file
+      rm -f "$file"
+    fi
+
+  fi
+  done
+
+  sleep 2
+  echo "$file"
+
+done
+
+) 9>/sql/process_pg_rtdata.exclusivelock
