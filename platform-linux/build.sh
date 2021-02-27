0e84f1a11641690a1b0d64ae40e81ac7bea86c8a
riclolsen <olsen.ricardo@gmail.com>
2021-02-27 18:25:09 -0300

    Added Linux build script.

---
+++ platform-linux/build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd ..
+mkdir bin
+
+cd src/lib60870.netcore
+dotnet publish --runtime linux-x64 -p:PublishReadyToRun=true -c Release -o ../../bin/
+
+cd ../OPC-UA-Client
+dotnet publish --runtime linux-x64 -p:PublishReadyToRun=true -c Release -o ../../bin/
+
+cd ../dnp3/Dnp3Client
+dotnet publish --runtime linux-x64 -p:PublishReadyToRun=true -c Release -o ../../../bin/
+
+export GOBIN=~/json-scada/bin
+cd ../../calculations
+go get ./...
+go build
+cp calculations ../../bin/
+
+cd ../i104m
+go get ./...
+go build
+cp i104m ../../bin/
+
+cd ../cs_data_processor
+npm update
+cd ../grafana_alert2event
+npm update
+cd ../demo_simul
+npm update
+cd ../server_realtime
+npm update
+cd ../server_realtime_auth
+npm update
+cd ../oshmi2json
+npm update
+cd ../oshmi_sync
+npm update
+cd ../htdocs-admin
+npm update
+npm run build
+cd ../shell_api
+npm update
+cd ../alarm_beep
+npm update
+
