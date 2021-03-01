2d264964569af7a28137559b4bc5e0b8c9167079
Ricardo Olsen <olsen.ricardo@gmail.com>
2021-03-01 17:22:37 -0300

    Tag edit dialog added.

--- src/htdocs-admin/src/components/Tags.vue
+++ src/htdocs-admin/src/components/Tags.vue
@@ -1,64 +1,190 @@
 <template>
-<div>
-  <v-container class="grey lighten-5">
-    <v-row no-gutters>
-      <v-col
-        cols="12"
-        sm="2"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-      <v-spacer></v-spacer>
-      <v-text-field
-        dense
-        @change="fetchTags"
-        v-model="searchTag"
-        append-icon="mdi-magnify"
-        label="Search Tag"
-        hide-details
-      ></v-text-field>
-        </v-card>
-      </v-col>
-      <v-col
-        cols="12"
-        sm="2"
-      >
-        <v-card
-          class="pa-2"
-          outlined
-          tile
-        >
-      <v-spacer></v-spacer>
-      <v-text-field
-        dense
-        @change="fetchTags"
-        v-model="searchDescription"
-        append-icon="mdi-magnify"
-        label="Description"
-        hide-details
-      ></v-text-field>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div>
 
-   <v-data-table
+    <v-data-table
       :headers="headers"
       :items="tags"
       :options.sync="options"
       :server-items-length="totalTags"
       :loading="loading"
-      :search="search"
       multi-sort
       class="elevation-1"
       dense
-      :items-per-page="15"
+      :items-per-page="15"      
     >
+        <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Tags</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+            <v-text-field
+              dense
+              @change="fetchTags"
+              v-model="searchTag"
+              append-icon="mdi-magnify"
+              label="Search Tag"
+              hide-details
+            ></v-text-field>
+        <v-spacer></v-spacer>
+
+            <v-text-field
+              dense
+              @change="fetchTags"
+              v-model="searchDescription"
+              append-icon="mdi-magnify"
+              label="Description"
+              hide-details
+            ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+
+      <v-dialog v-model="dialog" max-width="500px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark class="mb-2" v-bind="attrs" v-on="on">
+            New Tag
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Edit Tag</span>
+          </v-card-title>
+
+          <v-card-text>
+            <v-container>
+              <v-row dense>
+                <v-col cols="12" sm="6" md="12">
+                  <v-text-field dense
+                    v-model="editedItem.tag"
+                    label="Tag name"
+                  ></v-text-field>
+                  <v-text-field dense
+                    v-model="editedItem.description"
+                    label="Description"
+                  ></v-text-field>
+                  <v-text-field dense
+                    v-model="editedItem.group1"
+                    label="Group 1 (e.g.Installation/Plant)"
+                  ></v-text-field>
+                  <v-text-field dense
+                    v-model="editedItem.group2"
+                    label="Group 2 (e.g.Area/Bay)"
+                  ></v-text-field>
+                  <v-text-field dense 
+                    v-model="editedItem.group3"
+                    label="Group 3 (e.g.Equipment/Device)"
+                  ></v-text-field>
+                  <v-select
+                    :items="['supervised', 'command', 'calculated', 'manual' ]"
+                    label="Origin"
+                    v-model="editedItem.origin"
+                    class="ma-0"
+                  ></v-select>
+                  <v-select
+                    :items="['digital', 'analog', 'string' ]"
+                    label="Type"
+                    v-model="editedItem.type"
+                    class="ma-0"
+                  ></v-select>
+                  <v-text-field dense v-if="editedItem.type=='digital'"
+                    v-model="editedItem.stateTextFalse"
+                    label="State Text False"
+                  ></v-text-field>
+                  <v-text-field dense v-if="editedItem.type=='digital'"
+                    v-model="editedItem.stateTextTrue"
+                    label="State Text True"
+                  ></v-text-field>
+                  <v-text-field dense v-if="editedItem.type=='digital'"
+                    v-model="editedItem.eventTextFalse"
+                    label="Event Text False"
+                  ></v-text-field> 
+                  <v-text-field dense v-if="editedItem.type=='digital'"
+                    v-model="editedItem.eventTextTrue"
+                    label="Event Text True"
+                  ></v-text-field>
+                  <v-text-field dense v-if="editedItem.type=='analog'"
+                    v-model="editedItem.unit"
+                    label="Unit"
+                  ></v-text-field>
+
+                  <v-text-field dense type='number' v-if="['supervised'].includes(editedItem.origin)"
+                    v-model="editedItem.commandOfSupervised"
+                    label="Command point of supervised (use zero for none)"
+                  ></v-text-field>
+                  <v-text-field dense type='number' v-if="['command'].includes(editedItem.origin)"
+                    v-model="editedItem.supervisedOfCommand"
+                    label="Supervised point of command (use zero for none)"
+                  ></v-text-field>
+
+                  <v-text-field dense type='number' v-if="['supervised','command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceConnectionNumber"
+                    label="Protocol Source Connection Number"
+                  ></v-text-field>
+                  <v-text-field dense v-if="['supervised','command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceASDU"
+                    label="protocolSourceASDU"
+                  ></v-text-field>
+                  <v-text-field dense v-if="['supervised','command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceCommonAddress"
+                    label="protocolSourceCommonAddress"
+                  ></v-text-field>
+                  <v-text-field dense v-if="['supervised','command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceObjectAddress"
+                    label="protocolSourceObjectAddress"
+                  ></v-text-field>
+
+                  <v-text-field dense v-if="['command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceCommandDuration"
+                    label="protocolSourceCommandDuration"
+                  ></v-text-field>
+
+                  <v-switch dense v-if="['command'].includes(editedItem.origin)"
+                    v-model="editedItem.protocolSourceCommandUseSBO"
+                    inset
+                    color="primary"
+                    :label="`Use SBO: ${editedItem.protocolSourceCommandUseSBO?'true':'false'}`"
+                    class="mt-0"
+                  ></v-switch>
+
+
+                  <v-switch dense
+                    v-model="editedItem.isEvent"
+                    inset
+                    color="primary"
+                    :label="`Is event: ${editedItem.isEvent?'true':'false'}`"
+                    class="mt-0"
+                  ></v-switch>
+
+
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="close"> Cancel </v-btn>
+            <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      </v-toolbar>
+    </template>
+
+      <template v-slot:item.Actions="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
+        <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
+      </template>
     </v-data-table>
-    </div>
+  </div>
 </template>
 <script>
 // const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -68,6 +194,7 @@ export default {
 
   data: () => ({
     dialog: false,
+    dialogDelete: false,
     active: [],
     open: [],
     tags: [],
@@ -87,7 +214,19 @@ export default {
       { text: "Group1", sortable: true, value: "group1" },
       { text: "Description", sortable: true, value: "description" },
       { text: "Value", value: "value" },
+      { text: "Actions", value: "Actions", sortable: false },
     ],
+    editedIndex: -1,
+    editedItem: {
+      tag: "",
+      description: "",
+      group1: "",
+    },
+    defaultItem: {
+      tag: "",
+      description: "",
+      group1: "",
+    },
   }),
 
   computed: {
@@ -122,14 +261,48 @@ export default {
   },
 
   methods: {
+    editItem(item) {
+      console.log(item);
+      this.editedIndex = this.tags.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      this.editedIndex = this.tags.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialogDelete = true;
+    },
+    close() {
+      this.dialog = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+    closeDelete() {
+      this.dialogDelete = false;
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      });
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.tags[this.editedIndex], this.editedItem);
+      } else {
+        //  this.tags.push(this.editedItem);
+      }
+      this.close();
+    },
     async fetchTags() {
       const { sortBy, sortDesc, page, itemsPerPage } = this.options;
-      let filter = {}
+      let filter = {};
 
-      if (this.searchTag.trim()!="")
-        filter.tag =  {'$regex': this.searchTag, '$options': 'i'}
-      if (this.searchDescription.trim()!="")
-        filter.description =  {'$regex': this.searchDescription, '$options': 'i'}
+      if (this.searchTag.trim() != "")
+        filter.tag = { $regex: this.searchTag, $options: "i" };
+      if (this.searchDescription.trim() != "")
+        filter.description = { $regex: this.searchDescription, $options: "i" };
 
       this.loading = true;
       return await fetch("/Invoke/auth/listTags", {
@@ -143,7 +316,7 @@ export default {
           sortBy: sortBy,
           sortDesc: sortDesc,
           page: page,
-          filter: filter
+          filter: filter,
         }),
       })
         .then((res) => res.json())
