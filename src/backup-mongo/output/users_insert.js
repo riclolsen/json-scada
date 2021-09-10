94123eb2c9fe682f15a8ae2280c14946d4c599e7
Ricardo Olsen <olsen.ricardo@gmail.com>
2021-09-10 17:39:36 -0300

    New tool to create backup scripts for mongo shell.

--- src/backup-mongo/output/users_insert.js
+++
@@ -1,5 +0,0 @@
-db.users.insertOne({"_id":"5f6b4b38b2a89711f04dba0a","roles":["5f6640587c0983837b6d07a1","5f6640587c0983837b6d07a2"],"username":"admin","email":"admin@json-scada.org","password":"$2a$08$1AL80w2ToOk6HhXGHZoiDep/.NbVGrtXk8xkWF0Cxx9rxAMWF0F7C","__v":1});
-db.users.insertOne({"_id":"60fb31bcdc8d654284122545","username":"teste","email":"admin","password":"$2a$08$mnuYsyJRizk6QYmEGJeBg.fc2JSlLYaVwlGSTwuTLHzyIM6cLlZlu","roles":["5f6640587c0983837b6d07a2"],"__v":1});
-db.users.insertOne({"_id":"610a9912c2b0fe43dc6b6e66","username":"nome","password":"$2a$08$swCoDhURUOKN7YJD8Y88KOZrEY1tx93HnqI7rAJV3IpCnY.kYD75O","email":"1@2.com","roles":["5f6640587c0983837b6d07a2"],"__v":1});
-db.users.insertOne({"_id":"610a9beafd3a973074a269b3","username":"outro","password":"$2a$08$2BW3tY2E7znCdD6gt8VnAeHQTybA046XWk78RHEXe/UH4WMXWcmGK","email":"1@2.com","roles":["5f6640587c0983837b6d07a2"],"__v":1});
-db.users.insertOne({"_id":"610a9fb4fe19eb46fc2f5bac","username":"nerd","password":"$2a$08$dBCQfsxG9Pr6dOHd6apNJ.oqwdMzfFy64W.8YPW3zi4DOAgkPNZ3S","email":"","roles":[]});
