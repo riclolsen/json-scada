02119c88f0c117e9203869b98afd03ce7e788d19
Ricardo Olsen <olsen.ricardo@gmail.com>
2022-01-19 16:58:59 -0300

    Updated Windows create_services.bat.

--- platform-windows/create_services.bat
+++ platform-windows/create_services.bat
@@ -95,14 +95,14 @@ nssm set JSON_SCADA_php Start SERVICE_AUTO_START
 nssm install JSON_SCADA_nginx "c:\json-scada\platform-windows\nginx_php-runtime\nginx.exe" -c c:\json-scada\conf\nginx.conf
 nssm set JSON_SCADA_nginx Start SERVICE_AUTO_START
 
-REM SELECT THE DESIRED PROTOCOL DRIVERS (service startup options: SERVICE_AUTO_START, SERVICE_DELAYED_START, SERVICE_DEMAND_START, SERVICE_DISABLED)
+REM SELECT THE DESIRED PROTOCOL DRIVERS (service startup options: SERVICE_AUTO_START, SERVICE_DELAYED_AUTO_START, SERVICE_DEMAND_START, SERVICE_DISABLED)
 
 nssm install JSON_SCADA_mqttsparkplugclient "C:\json-scada\platform-windows\nodejs-runtime\node.exe" "C:\json-scada\src\mqtt-sparkplug\index.js" 
 nssm set JSON_SCADA_mqttsparkplugclient AppDirectory "C:\json-scada\src\mqtt-sparkplug"
-nssm set JSON_SCADA_mqttsparkplugclient Start SERVICE_AUTO_START
+nssm set JSON_SCADA_mqttsparkplugclient Start SERVICE_DELAYED_AUTO_START
 
 nssm install JSON_SCADA_iec104client "C:\json-scada\bin\iec104client.exe" 1 1 
-nssm set JSON_SCADA_iec104client Start SERVICE_DELAYED_START
+nssm set JSON_SCADA_iec104client Start SERVICE_DELAYED_AUTO_START
 
 nssm install JSON_SCADA_iec104server "C:\json-scada\bin\iec104server.exe" 1 1
 nssm set JSON_SCADA_iec104server Start SERVICE_DEMAND_START
