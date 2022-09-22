83ccbd36cec453c01b7661a78820dadcfb4c1250
Ricardo Olsen <olsen.ricardo@gmail.com>
2022-09-22 12:07:01 -0300

    Version 0.26-alpha.

--- src/htdocs/i18n/messages_i18n-pt_br.js
+++ src/htdocs/i18n/messages_i18n-pt_br.js
@@ -4,7 +4,7 @@
 var Msg =
 {
 NomeProduto: '{json:scada}',
-VersaoProduto: '0.25',
+VersaoProduto: '0.26',
     
 NomeVisorTelas:       'Visor de Telas',
 NomeVisorEventos:     'Visor de Eventos',
--- src/htdocs/i18n/messages_i18n-uk_ua.js
+++ src/htdocs/i18n/messages_i18n-uk_ua.js
@@ -4,7 +4,7 @@
 var Msg =
 {
 NomeProduto: '{json:scada}',
-VersaoProduto: '0.25',
+VersaoProduto: '0.26',
 
 NomeVisorTelas: 'Перегляд ',
 NomeVisorEventos: 'Події',
