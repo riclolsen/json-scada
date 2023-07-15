551fbc30b2d7fe08c77ba5878dbc0aea4aa79576
Ricardo Olsen <olsen.ricardo@gmail.com>
2023-07-15 17:47:13 -0300

    Refactored config server for excel module.

--- src/config_server_for_excel/index.js
+++ src/config_server_for_excel/index.js
@@ -1,34 +1,52 @@
+'use strict'
+
+/*
+ * {json:scada} - Copyright (c) 2020-2023 - Ricardo L. Olsen
+ * This file is part of the JSON-SCADA distribution (https://github.com/riclolsen/json-scada).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const express = require('express')
-const mongo = require('mongodb')
-const { MongoClient, ReadPreference } = require('mongodb')
+const { MongoClient, Double } = require('mongodb')
 const { parse } = require('json2csv')
 const Log = require('./simple-logger')
 const AppDefs = require('./app-defs')
 const LoadConfig = require('./load-config')
 const app = express()
 
-function toMongoDouble (value, defaultValue) {
+function toMongoDouble(value, defaultValue) {
   defaultValue = defaultValue || 0
   if (value === undefined || value === null) value = defaultValue
-  if (typeof value === 'number') return new mongo.Double(value)
+  if (typeof value === 'number') return new Double(value)
   value = parseFloat(value)
-  if (isNaN(value)) value = new mongo.Double(parseFloat(defaultValue))
-  else value = new mongo.Double(value)
+  if (isNaN(value)) value = new Double(parseFloat(defaultValue))
+  else value = new Double(value)
   return value
 }
 
-function toMongoDoubleOrString (value, defaultValue) {
-  defaultValue = defaultValue || ""
+function toMongoDoubleOrString(value, defaultValue) {
+  defaultValue = defaultValue || ''
   if (value === undefined || value === null) value = defaultValue
-  if (typeof value === 'number') return new mongo.Double(value)
+  if (typeof value === 'number') return new Double(value)
   let v = parseFloat(value)
-  console.log(value)
+  Log.log(value)
   if (isNaN(v)) value = value.trim()
-  else value = new mongo.Double(v)
+  else value = new Double(v)
   return value
 }
 
-function toMongoBoolean (value, defaultValue) {
+function toMongoBoolean(value, defaultValue) {
   if (typeof defaultValue !== 'boolean') defaultValue = false
   if (value === undefined || value === null) value = defaultValue
   if (typeof value === 'boolean') return value
@@ -39,7 +57,7 @@ function toMongoBoolean (value, defaultValue) {
   return value
 }
 
-function toMongoObj (value, defaultValue) {
+function toMongoObj(value, defaultValue) {
   defaultValue = defaultValue || {}
   if (value === undefined || value === null) value = defaultValue
   if (typeof value === 'object') return value
@@ -67,8 +85,8 @@ Log.levelCurrent = jsConfig.LogLevel
 app.use(express.json({ limit: '200mb' }))
 app.use(express.text({ limit: '200mb' }))
 
-const IP_BIND = process.env.JS_CSEXCEL_IP_BIND || AppDefs.IP_BIND;
-const HTTP_PORT = process.env.JS_CSEXCEL_HTTP_PORT || AppDefs.HTTP_PORT;
+const IP_BIND = process.env.JS_CSEXCEL_IP_BIND || AppDefs.IP_BIND
+const HTTP_PORT = process.env.JS_CSEXCEL_HTTP_PORT || AppDefs.HTTP_PORT
 
 app.listen(HTTP_PORT, IP_BIND, () => {
   Log.log('listening on ' + IP_BIND + ':' + HTTP_PORT)
@@ -77,13 +95,13 @@ app.listen(HTTP_PORT, IP_BIND, () => {
 MongoClient.connect(
   // try to (re)connect
   jsConfig.mongoConnectionString,
-  getMongoConnectionOptions(jsConfig)
-).then(async client => {
+  jsConfig.MongoConnectionOptions
+).then(async (client) => {
   Log.log('MongoDB - Connected correctly to MongoDB server', Log.levelMin)
   const db = client.db(jsConfig.mongoDatabaseName)
-  connsCollection = db.collection(jsConfig.ProtocolConnectionsCollectionName)
-  rtDataCollection = db.collection(jsConfig.RealtimeDataCollectionName)
-  instCollection = db.collection(jsConfig.ProtocolDriverInstancesCollectionName)
+  const connsCollection = db.collection(jsConfig.ProtocolConnectionsCollectionName)
+  const rtDataCollection = db.collection(jsConfig.RealtimeDataCollectionName)
+  const instCollection = db.collection(jsConfig.ProtocolDriverInstancesCollectionName)
 
   let instFields = [
     '_id',
@@ -92,10 +110,10 @@ MongoClient.connect(
     'enabled',
     'logLevel',
     'nodeNames',
-    'keepProtocolRunningWhileInactive'
+    'keepProtocolRunningWhileInactive',
   ]
   let instProjection = {}
-  instFields.forEach(value => {
+  instFields.forEach((value) => {
     instProjection[value] = 1
   })
 
@@ -105,6 +123,10 @@ MongoClient.connect(
       .project(instProjection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.setHeader('content-type', 'text/csv')
     res.setHeader('header', 'present')
     res.send(parse(results, { fields: instFields }))
@@ -116,6 +138,10 @@ MongoClient.connect(
       .project(instProjection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.send(results)
   })
 
@@ -127,10 +153,10 @@ MongoClient.connect(
     'name',
     'description',
     'enabled',
-    'commandsEnabled'
+    'commandsEnabled',
   ]
   let connsProjection = {}
-  connsFields.forEach(value => {
+  connsFields.forEach((value) => {
     connsProjection[value] = 1
   })
 
@@ -140,6 +166,10 @@ MongoClient.connect(
       .project(connsProjection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.setHeader('content-type', 'text/csv')
     res.setHeader('header', 'present')
     res.send(parse(results, { fields: connsFields }))
@@ -151,6 +181,10 @@ MongoClient.connect(
       .project(connsProjection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.send(results)
   })
 
@@ -201,10 +235,10 @@ MongoClient.connect(
     'hysteresis',
     'alarmDisabled',
     'commandBlocked',
-    'commissioningRemarks'
+    'commissioningRemarks',
   ]
   let projection = {}
-  fields.forEach(value => {
+  fields.forEach((value) => {
     projection[value] = 1
   })
 
@@ -214,6 +248,10 @@ MongoClient.connect(
       .project(projection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.setHeader('content-type', 'text/csv')
     res.setHeader('header', 'present')
     res.send(parse(results, { fields }))
@@ -225,15 +263,19 @@ MongoClient.connect(
       .project(projection)
       .sort({ _id: 1 })
       .toArray()
+      .catch(function (err) {
+        Log.log(err)
+      })
+
     res.send(results)
   })
 
   app.post('/excel/realtimeDataUpdate', async function (req, res) {
-    console.log(req.body)
+    Log.log(req.body)
     let data = JSON.parse(req.body)
 
-    await data.forEach(async elem => {
-      console.log(elem.tag)
+    await data.forEach(async (elem) => {
+      Log.log(elem.tag)
 
       let _id = toMongoDouble(elem._id)
       elem.tag = elem.tag || ''
@@ -287,19 +329,44 @@ MongoClient.connect(
         false
       )
       elem.protocolDestinations = toMongoObj(elem.protocolDestinations, [])
-      elem.protocolDestinations.forEach( pd => {
-        pd.protocolDestinationConnectionNumber = toMongoDouble(pd.protocolDestinationConnectionNumber)
-        pd.protocolDestinationCommonAddress = toMongoDouble(pd.protocolDestinationCommonAddress)
-        pd.protocolDestinationObjectAddress = toMongoDoubleOrString(pd.protocolDestinationObjectAddress)
-        pd.protocolDestinationASDU = toMongoDoubleOrString(pd.protocolDestinationASDU)
-        pd.protocolDestinationCommandDuration = toMongoDouble(pd.protocolDestinationCommandDuration)
-        pd.protocolDestinationCommandUseSBO = toMongoBoolean(pd.protocolDestinationCommandUseSBO, false)
-        pd.protocolDestinationKConv1 = toMongoDouble(pd.protocolDestinationKConv1, 1)
-        pd.protocolDestinationKConv2 = toMongoDouble(pd.protocolDestinationKConv2, 0)
-        pd.protocolDestinationGroup = toMongoDouble(pd.protocolDestinationGroup, 0)
-        pd.protocolDestinationHoursShift = toMongoDouble(pd.protocolDestinationHoursShift, 0)
-        } )
-      
+      elem.protocolDestinations.forEach((pd) => {
+        pd.protocolDestinationConnectionNumber = toMongoDouble(
+          pd.protocolDestinationConnectionNumber
+        )
+        pd.protocolDestinationCommonAddress = toMongoDouble(
+          pd.protocolDestinationCommonAddress
+        )
+        pd.protocolDestinationObjectAddress = toMongoDoubleOrString(
+          pd.protocolDestinationObjectAddress
+        )
+        pd.protocolDestinationASDU = toMongoDoubleOrString(
+          pd.protocolDestinationASDU
+        )
+        pd.protocolDestinationCommandDuration = toMongoDouble(
+          pd.protocolDestinationCommandDuration
+        )
+        pd.protocolDestinationCommandUseSBO = toMongoBoolean(
+          pd.protocolDestinationCommandUseSBO,
+          false
+        )
+        pd.protocolDestinationKConv1 = toMongoDouble(
+          pd.protocolDestinationKConv1,
+          1
+        )
+        pd.protocolDestinationKConv2 = toMongoDouble(
+          pd.protocolDestinationKConv2,
+          0
+        )
+        pd.protocolDestinationGroup = toMongoDouble(
+          pd.protocolDestinationGroup,
+          0
+        )
+        pd.protocolDestinationHoursShift = toMongoDouble(
+          pd.protocolDestinationHoursShift,
+          0
+        )
+      })
+
       elem.hiLimit = toMongoDouble(elem.hiLimit, Number.MAX_VALUE)
       elem.hihiLimit = toMongoDouble(elem.hihiLimit, Number.MAX_VALUE)
       elem.hihihiLimit = toMongoDouble(elem.hihihiLimit, Number.MAX_VALUE)
@@ -312,7 +379,7 @@ MongoClient.connect(
       delete elem._id
 
       let onInsert = {
-        value: new mongo.Double(0),
+        value: new Double(0),
         valueString: '',
         valueJson: {},
         sourceDataUpdate: null,
@@ -328,63 +395,34 @@ MongoClient.connect(
         timeTagAlarm: null,
         timeTagAtSource: null,
         timeTagAtSourceOk: false,
-        updatesCnt: new mongo.Double(0)
+        updatesCnt: new Double(0),
       }
-      
 
       if (elem.tag.trim() === '') {
-        Log.log("DELETE _id=" + _id)
-        let result = await rtDataCollection.deleteOne(
-          { _id: _id },
-        )
+        Log.log('DELETE _id=' + _id)
+        let result = await rtDataCollection.deleteOne({ _id: _id })
+        Log.log(result)
       } else {
-        Log.log("UPDATE _id=" + _id + ' tag=' + elem.tag)
+        Log.log('UPDATE _id=' + _id + ' tag=' + elem.tag)
         let result = await rtDataCollection.updateOne(
           { _id: _id },
           { $set: elem, $setOnInsert: onInsert },
           { upsert: true }
         )
-        console.log(elem)
-        console.log(`matched ${result.matchedCount} modified ${result.modifiedCount} upserted: ${result.upsertedCount}` )
-        
+        .catch(function (err) {
+          Log.log(err)
+        })
+
+        Log.log(elem)
+        Log.log(
+          `matched ${result.matchedCount} modified ${result.modifiedCount} upserted: ${result.upsertedCount}`
+        )
       }
     })
-    Log.log("Response 200")
+    Log.log('Response 200')
     res.sendStatus(200)
   })
 })
-
-function getMongoConnectionOptions (configObj) {
-  let connOptions = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    appname:
-      AppDefs.NAME +
-      ' Version:' +
-      AppDefs.VERSION +
-      ' Instance:' +
-      configObj.Instance,
-    maxPoolSize: 20,
-    readPreference: ReadPreference.PRIMARY
-  }
-
-  if (
-    typeof configObj.tlsCaPemFile === 'string' &&
-    configObj.tlsCaPemFile.trim() !== ''
-  ) {
-    configObj.tlsClientKeyPassword = configObj.tlsClientKeyPassword || ''
-    configObj.tlsAllowInvalidHostnames =
-      configObj.tlsAllowInvalidHostnames || false
-    configObj.tlsAllowChainErrors = configObj.tlsAllowChainErrors || false
-    configObj.tlsInsecure = configObj.tlsInsecure || false
-
-    connOptions.tls = true
-    connOptions.tlsCAFile = configObj.tlsCaPemFile
-    connOptions.tlsCertificateKeyFile = configObj.tlsClientPemFile
-    connOptions.tlsCertificateKeyFilePassword = configObj.tlsClientKeyPassword
-    connOptions.tlsAllowInvalidHostnames = configObj.tlsAllowInvalidHostnames
-    connOptions.tlsInsecure = configObj.tlsInsecure
-  }
-
-  return connOptions
-}
+.catch(function (err) {
+  Log.log(err)
+})
