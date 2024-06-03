e956643b94530f5788eb8de25b833f6462f78602
Ricardo Olsen <olsen.ricardo@gmail.com>
2024-06-03 20:41:58 -0300

    Compilation script for Inkscape+SAGE on RHEL9.

--- platform-rhel9/inkscape-plus-sage.sh
+++ platform-rhel9/inkscape-plus-sage.sh
@@ -7,7 +7,7 @@ JS_USERNAME=jsonscada
 cd /home/jsonscada
 sudo -u $JS_USERNAME sh -c 'git clone --recurse-submodules https://gitlab.com/ricardolo/inkscape-rebased.git'
 cd inkscape-rebased
-#sudo -u $JS_USERNAME sh -c 'mkdir build'
+sudo -u $JS_USERNAME sh -c 'mkdir build'
 cd build
 #sudo -u $JS_USERNAME sh -c 'cmake -DENABLE_POPPLER_CAIRO=OFF -DCMAKE_CXX_STANDARD=20 ..'
 #sudo -u $JS_USERNAME sh -c 'make'
