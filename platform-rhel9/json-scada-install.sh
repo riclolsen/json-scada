dc66fb7d2d4c9f9c881458b4e09676ff6e91a3e9
Ricardo Olsen <olsen.ricardo@gmail.com>
2024-06-04 13:15:26 -0300

    Script for installation on RHEL9 updated.

--- platform-rhel9/json-scada-install.sh
+++ platform-rhel9/json-scada-install.sh
@@ -26,11 +26,11 @@ sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.
 sudo dnf -y install epel-release 
 sudo dnf -y install tar vim nano nginx wget chkconfig dotnet-sdk-6.0 java-21-openjdk php curl
 # to compile inkscape
-wget https://dl.rockylinux.org/pub/rocky/9/devel/x86_64/os/Packages/p/potrace-devel-1.16-7.el9.x86_64.rpm
-sudo rpm -ivh potrace-devel-1.16-7.el9.x86_64.rpm
 sudo dnf -y install ninja-build libjpeg-devel libxslt-devel gtkmm30-devel gspell-devel boost-devel poppler-devel poppler-glib-devel gtest-devel harfbuzz-devel 
 sudo dnf -y install libwpg-devel librevenge-devel libvisio-devel libcdr-devel readline-devel ImageMagick-c++-devel GraphicsMagick-c++-devel
 sudo dnf -y install pango-devel gsl-devel libsoup-devel lcms2-devel gc-devel double-conversion-devel potrace python3-scour
+wget https://dl.rockylinux.org/pub/rocky/9/devel/x86_64/os/Packages/p/potrace-devel-1.16-7.el9.x86_64.rpm
+sudo dnf -y install ./potrace-devel-1.16-7.el9.x86_64.rpm
 
 sudo update-crypto-policies --set LEGACY
 
