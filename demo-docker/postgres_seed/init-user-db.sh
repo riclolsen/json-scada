736d6d2b22655e832518479fa4e75fc03673fcc2
Ricardo Olsen <olsen.ricardo@gmail.com>
2024-09-15 22:48:33 -0300

    Fix docker demo postgres seed.

--- demo-docker/postgres_seed/init-user-db.sh
+++ demo-docker/postgres_seed/init-user-db.sh
@@ -1,2 +1,5 @@
-psql -h 127.0.0.1 -U postgres -w -f data/create_tables.sql template1 && psql -h 127.0.0.1 -U postgres -w -f data/metabaseappdb.sql metabaseappdb && psql -h 127.0.0.1 -U postgres -w -f data/grafanaappdb.sql grafanaappdb
+#!/bin/bash
+psql -h 127.0.0.1 -U postgres -w -f data/create_tables.sql template1
+psql -h 127.0.0.1 -U postgres -w -f data/metabaseappdb.sql metabaseappdb 
+psql -h 127.0.0.1 -U postgres -w -f data/grafanaappdb.sql grafanaappdb
 
