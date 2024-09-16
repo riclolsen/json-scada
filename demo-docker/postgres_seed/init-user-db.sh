7b9135bc0e57760f4a3a9a5143eea911b1f1a7bb
Ricardo Olsen <olsen.ricardo@gmail.com>
2024-09-15 22:17:17 -0300

    Moved grafana and metabase to docker demo postgres seed.

--- demo-docker/postgres_seed/init-user-db.sh
+++ demo-docker/postgres_seed/init-user-db.sh
@@ -1,4 +1,2 @@
-psql -h 127.0.0.1 -U postgres -w -f create_tables.sql template1
-psql -h 127.0.0.1 -U postgres -w -f metabaseappdb.sql metabaseappdb
-psql -h 127.0.0.1 -U postgres -w -f grafanaappdb.sql grafanaappdb
+psql -h 127.0.0.1 -U postgres -w -f data/create_tables.sql template1 && psql -h 127.0.0.1 -U postgres -w -f data/metabaseappdb.sql metabaseappdb && psql -h 127.0.0.1 -U postgres -w -f data/grafanaappdb.sql grafanaappdb
 
