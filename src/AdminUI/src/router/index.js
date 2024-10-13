82c9f308023cf03073daefdfe04bdff6468d1a28
Ricardo Olsen <olsen.ricardo@gmail.com>
2024-10-13 16:28:29 -0300

    Updated new AdminUI i18n.

--- src/AdminUI/src/router/index.js
+++ src/AdminUI/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import LoginPage from '../components/LoginPage.vue'
 import DashboardPage from '../components/DashboardPage.vue'
 import AboutPage from '../components/AboutPage.vue'
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
