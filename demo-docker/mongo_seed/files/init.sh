937395ef8c2dec5f214792107bfbcaa636a12822
riclolsen <riclolsen@github.com>
2024-12-15 09:17:07 -0300

    Fix config for OPC-UA-Client to docker demo.

--- demo-docker/mongo_seed/files/init.sh
+++ demo-docker/mongo_seed/files/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 sleep 5 
 mongoimport --db $MONGO_INITDB_DATABASE --collection protocolDriverInstances --type json --file /docker-entrypoint-initdb.d/demo_instances.json 
-mongoimport --db $MONGO_INITDB_DATABASE --collection protocolConnections --type json --file /docker-entrypoint-initdb.d/demo_connections.json 
+mongoimport --db $MONGO_INITDB_DATABASE --collection protocolConnections --type json --file /docker-entrypoint-initdb.d/demo_connections_linux.json 
 mongoimport --db $MONGO_INITDB_DATABASE --collection realtimeData --type json --file /docker-entrypoint-initdb.d/demo_data.json 
 mongoimport --db $MONGO_INITDB_DATABASE --collection processInstances --type json --file /docker-entrypoint-initdb.d/demo_process_instances.json 
 mongoimport --db $MONGO_INITDB_DATABASE --collection users --type json --file /docker-entrypoint-initdb.d/demo_users.json 
