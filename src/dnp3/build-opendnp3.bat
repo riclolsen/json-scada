d2ad50aa765d50f6d820532c8fb85d47fd65f889
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-01-20 09:31:54 -0300

    Update opendnp3 build instructions for Windows.

--- src/dnp3/build-opendnp3.bat
+++ src/dnp3/build-opendnp3.bat
@@ -1,5 +1,5 @@
 REM Requires https://slproweb.com/products/Win32OpenSSL.html (not the light package)
-REM https://slproweb.com/download/Win64OpenSSL-1_1_1k.msi
+REM https://slproweb.com/download/Win64OpenSSL-3_4_0.msi
 
 git clone https://github.com/dnp3/opendnp3
 
@@ -8,7 +8,6 @@ cd opendnp3
 mkdir build
 cd build
 
-SET OPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64"
-cmake -DDNP3_DEMO=ON -DDNP3_TLS=ON -DDNP3_DOTNET=ON ..
+cmake -DDNP3_DEMO=ON -DDNP3_TLS=ON -DDNP3_DOTNET=ON -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64" -DOPENSSL_USE_STATIC_LIBS=TRUE -DOPENSSL_MSVC_STATIC_RT=TRUE ..
 
 echo now run "msbuild opendnp3.sln /p:Configuration=Release" or open opendnp3.sln on Visual Studio.
