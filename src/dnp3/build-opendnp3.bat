7fa811011eeb2f8b381aa386c118346aec3ba739
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-01-22 20:05:09 -0300

    Added instructions to setup OpenDnp3 and MongoDB driver for C++ dev.

--- src/dnp3/build-opendnp3.bat
+++ src/dnp3/build-opendnp3.bat
@@ -8,6 +8,6 @@ cd opendnp3
 mkdir build
 cd build
 
-cmake -DDNP3_EXAMPLES=ON -DDNP3_TLS=ON -DDNP3_DOTNET=ON -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64" -DOPENSSL_USE_STATIC_LIBS=TRUE -DOPENSSL_MSVC_STATIC_RT=TRUE ..
+cmake -DDNP3_JSONSCADA=ON -DDNP3_EXAMPLES=ON -DDNP3_TLS=ON -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64" -DOPENSSL_USE_STATIC_LIBS=TRUE -DOPENSSL_MSVC_STATIC_RT=TRUE ..
 
 echo now run "msbuild opendnp3.sln /p:Configuration=Release" or open opendnp3.sln on Visual Studio.
