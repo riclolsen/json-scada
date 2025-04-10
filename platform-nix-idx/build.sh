868c5352d2de4e07d4cbc6b420ea6a496436f321
riclolsen <riclolsen@github.com>
2025-04-10 10:09:09 -0300

    Add Go flags to find CGO include/lib dirs.

--- platform-nix-idx/build.sh
+++ platform-nix-idx/build.sh
@@ -70,6 +70,8 @@ dotnet publish --no-self-contained --runtime $ARG1 -p:PublishReadyToRun=true -c 
 
 export GOBIN=~/json-scada/bin
 go env -w GO111MODULE=auto
+export CGO_CPPFLAGS="-I /usr/include"
+export CGO_LDFLAGS="-L /usr/lib"
 
 cd ../calculations
 go mod tidy 
