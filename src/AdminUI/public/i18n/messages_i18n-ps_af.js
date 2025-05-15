862bd28a4fb92f14e8e59304bb9107bc2a6f53e3
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-05-15 15:48:03 -0300

    Version 0.52-alpha.

--- src/AdminUI/public/i18n/messages_i18n-ps_af.js
+++ src/AdminUI/public/i18n/messages_i18n-ps_af.js
@@ -3,7 +3,7 @@
 
 var Msg = {
     NomeProduto: '{json:scada}',
-    VersaoProduto: '0.51',
+    VersaoProduto: '0.52',
 
     NomeVisorTelas: 'د نښته کتنې لیدل',
     NomeVisorEventos: 'د واقعو کتنې لیدل',
--- src/AdminUI/public/i18n/messages_i18n-pt_br.js
+++ src/AdminUI/public/i18n/messages_i18n-pt_br.js
@@ -4,7 +4,7 @@
 var Msg =
 {
 NomeProduto: '{json:scada}',
-VersaoProduto: '0.51',
+VersaoProduto: '0.52',
     
 NomeVisorTelas:       'Visor de Telas',
 NomeVisorEventos:     'Visor de Eventos',
