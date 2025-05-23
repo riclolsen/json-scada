3a482a1575bd822b9c7a5ffaf01ba44579643ce3
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-05-23 10:52:00 -0300

    Nix platform: update postgresql to version 15.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,9 +7,9 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [   
     pkgs.sudo
-    pkgs.postgresql_14_jit
-    pkgs.postgresql14Packages.timescaledb
-    pkgs.postgresql14Packages.timescaledb_toolkit
+    pkgs.postgresql_15_jit
+    pkgs.postgresql15Packages.timescaledb
+    pkgs.postgresql15Packages.timescaledb_toolkit
     pkgs.util-linux.bin
     pkgs.dotnet-sdk_8
     pkgs.vscode-extensions.ms-dotnettools.csharp
