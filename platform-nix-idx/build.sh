bf11daf33fd35db02e4c9b15fd238e999ba17e11
Ricardo Olsen <olsen.ricardo@gmail.com>
2025-07-11 16:11:47 -0300

    Release more storage space on Firebase platform.

--- platform-nix-idx/build.sh
+++ platform-nix-idx/build.sh
@@ -12,6 +12,9 @@ ARG1=${1:-linux-x64}
 # extract submodules for mongodb C++ driver and opendnp3 (may take too much storage space)
 #git submodule update --init --recursive
 
+# remove flutter to release space (flutter is not used in this project)
+rm -rf ~/flutter
+
 cd ..
 mkdir bin
 mkdir bin-wine
@@ -48,8 +51,14 @@ dotnet publish --no-self-contained --runtime $ARG1 -p:PublishReadyToRun=true -c 
 #dotnet restore
 #dotnet publish --no-self-contained --runtime win-x64 -p:PublishReadyToRun=true -f net8.0-windows -c Release -o ../../bin-wine/ OPC-DA-Client.csproj
 
+#git submodule update --init --recursive
 #cd ../mongo-cxx-driver/mongo-cxx-driver/build
-#cmake .. -DCMAKE_INSTALL_PREFIX="../../../mongo-cxx-driver-lib" -DCMAKE_CXX_STANDARD=17 -DBUILD_VERSION=4.0.0 -DBUILD_SHARED_LIBS=OFF -DBUILD_SHARED_AND_STATIC_LIBS=OFF  -DOPENSSL_INCLUDE_DIR=/nix/store/jv45xs1p8v9mcychfgkv6vxridcn532h-openssl-3.4.1-dev/include/
+#nix-shell -p openssl
+#nix-shell -p snappy
+#nix-shell -p zstd
+#nix-shell -p pkgconf
+#cmake .. -DCMAKE_INSTALL_PREFIX="../../../mongo-cxx-driver-lib" -DCMAKE_CXX_STANDARD=17 -DBUILD_VERSION=4.0.0 -DBUILD_SHARED_LIBS=OFF -DBUILD_SHARED_AND_STATIC_LIBS=OFF  -DOPENSSL_INCLUDE_DIR=/nix/store/jv45xs1p8v9mcychfgkv6vxridcn532h-openssl-3.4.1-dev/include/ -DOPENSSL_ROOT_DIR=/nix/store/jv45xs1p8v9mcychfgkv6vxridcn532h-openssl-3.4.1-dev/ -DCMAKE_INCLUDE_PATH=/nix/store/l5ghfypzbn253233wkxhija58nlpdg4g-snappy-1.1.10-dev/include/ -DCMAKE_LIBRARY_PATH=/nix/store/l5ghfypzbn253233wkxhija58nlpdg4g-snappy-1.1.10-dev/ 
+## -DLIBRESSL_CRYPTO_LIBRARY=/nix/store/ip844kbgzqrkhg42g2m9ghygfkyamrw2-libressl-3.8.4-dev/ -DLIBRESSL_INCLUDE_DIR=/nix/store/ip844kbgzqrkhg42g2m9ghygfkyamrw2-libressl-3.8.4-dev/include/
 #cmake --build . --config Release
 #cmake --build . --target install --config Release
 #
@@ -74,6 +83,8 @@ export CGO_CPPFLAGS="-I /usr/include"
 export CGO_LDFLAGS="-L /usr/lib"
 
 # you may need a lot of memory to build go drivers, the build may be killed by the system, if necessary add swap, e.g. 8GB RAM + 4GB Swap
+nix-shell -p libpcap
+
 cd ../calculations
 go mod tidy 
 go build
