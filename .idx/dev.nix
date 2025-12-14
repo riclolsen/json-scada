c6f637ea0ad78002df4bf9919d4bb98dcaf9e1a0
riclolsen <riclolsen@github.com>
2025-12-14 10:57:42 +0100

    Disable web preview in dev.nix.

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -66,16 +66,16 @@
     previews = {
       enable = true;
       previews = {
-        web = {
-          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-          # and show it in IDX's web preview panel
-          # command = ["npm" "run" "dev"];
-          manager = "web";
-          env = {
-            # Environment variables to set for your server
-            PORT = "8080";
-          };
-        };
+        #web = {
+        #  # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #  # and show it in IDX's web preview panel
+        #  # command = ["npm" "run" "dev"];
+        #  manager = "web";
+        #  env = {
+        #    # Environment variables to set for your server
+        #    PORT = "8080";
+        #  };
+        #};
       };
     };
 
